29ed268a95058bb994ab27c01b5cc452f82f3d09
= <=>
2013-01-27 09:48:45 -0700

    Updated copyright years.

--- FlitBit.IoC/ContainerRegisterAttribute.cs
+++ FlitBit.IoC/ContainerRegisterAttribute.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿#region COPYRIGHT© 2009-2013 Phillip Clark. All rights reserved.
+// For licensing information see License.txt (MIT style licensing).
+#endregion
+
+using System;
 using System.Diagnostics.Contracts;
 using System.Reflection;
 using FlitBit.Core;
--- FlitBit.IoC/ModuleWireupTask.cs
+++ FlitBit.IoC/ModuleWireupTask.cs
@@ -1,4 +1,8 @@
-﻿using FlitBit.Wireup.Meta;
+﻿#region COPYRIGHT© 2009-2013 Phillip Clark. All rights reserved.
+// For licensing information see License.txt (MIT style licensing).
+#endregion
+
+using FlitBit.Wireup.Meta;
 
 [assembly: FlitBit.IoC.ModuleWireupTask]
 
