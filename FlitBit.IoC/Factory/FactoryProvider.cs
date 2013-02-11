12fa72d466904dd1ca036d93eef3450f891ba0c0
cerebralkungfu <phillip@flitbit.com>
2013-02-10 19:43:40 -0700

    Added FactoryProvider to replace the default factory provider with one that returns containers.

---
+++ FlitBit.IoC/Factory/FactoryProvider.cs
@@ -0,0 +1,15 @@
+﻿using FlitBit.Core.Factory;
+
+namespace FlitBit.IoC.Factory
+{
+	/// <summary>
+	/// Factory provider implementation that returns the current IContainer.
+	/// </summary>
+	internal class FactoryProvider : IFactoryProvider
+	{
+		public IFactory GetFactory()
+		{
+			return Container.Current;
+		}
+	}
+}
---
+++ FlitBit.IoC/Wireup.cs
@@ -0,0 +1,24 @@
+﻿using FlitBit.Core;
+using FlitBit.Wireup;
+using FlitBit.Wireup.Meta;
+using System;
+
+[assembly: Wireup(typeof(FlitBit.IoC.WireupThisAssembly))]
+
+namespace FlitBit.IoC
+{
+	/// <summary>
+	/// Wires up this assembly.
+	/// </summary>
+	public sealed class WireupThisAssembly : IWireupCommand
+	{
+		/// <summary>
+		/// Wires up this assembly.
+		/// </summary>
+		/// <param name="coordinator"></param>
+		public void Execute(IWireupCoordinator coordinator)
+		{
+			FactoryProvider.SetFactoryProvider(new FlitBit.IoC.Factory.FactoryProvider());
+		}
+	}
+}
